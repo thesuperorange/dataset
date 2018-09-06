b16de9f300ecde04008b776377c61222873f93fd
Your Name <you@example.com>
2018-09-06 14:53:37 +0800

    basic

--- 0.1_basic.R
+++ 0.1_basic.R
@@ -9,3 +9,17 @@ credit = read.df("dataset/Log_reg_credit.csv","csv", header=T, inferSchema = T)
 group_major = count(groupBy(credit,credit$Major))
 collect(arrange(group_major,desc(group_major$count)))
 filter(credit,credit$GPA>3)
+
+
+appx = select(credit,approxCountDistinct(credit$Gender))
+collect(appx)
+
+credit_rm = withColumnRenamed(credit, "OBS",  "num")
+head(credit_rm)
+collect(people[people$age>20])
+
+collect(people)
+collect(dropna(people))
+collect(fillna(people,list(age=30)))
+
+
